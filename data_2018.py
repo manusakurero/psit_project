f7962db3caadf08309e98b7f192a567c81caea77
SarunyuRuenruay <sarunyu.n17@gmail.com>
2018-11-27 20:37:33 +0700

    chang local

--- data_2018.py
+++ data_2018.py
@@ -2,10 +2,7 @@
 import csv
 def table2018():
     """input file as local"""
-    #where is your csv file
-    local_file = 'D:\\OVERFOOL\\bgg_db_2018_01.csv'
-    #local = r'D:\OVERFOOL\bgg_db_2018_01.csv'
-    with open(local_file) as csvfile:
+    with open("bgg_db_2018_01.csv", "r") as csvfile:
         data = csv.reader(csvfile)
         table = [row for row in data]
         return table
