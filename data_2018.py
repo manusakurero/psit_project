07391b5e1d6aebcc9e911d0882e64d04ce594b7a
SarunyuRuenruay <sarunyu.n17@gmail.com>
2018-12-12 18:05:09 +0700

    add encode and local

--- data_2018.py
+++ data_2018.py
@@ -3,7 +3,7 @@ import csv
 def table2018():
     """input file as local"""
     #where is your csv file
-    local_file = 'D:\\OVERFOOL\\bgg_db_2018_01.csv'
+    local_file = 'bgg_db_2018_01.csv'
     #local = r'D:\OVERFOOL\bgg_db_2018_01.csv'
     with open(local_file, "r", encoding='latin-1') as csvfile:
         data = csv.reader(csvfile)
